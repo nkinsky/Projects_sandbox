024805b4a595e6a6fafa02e658132b6763d444cd
Nat Kinsky <nkinsky@umich.edu>
2025-12-03 13:54:35 -0500

    debug - backwards / forward compatibility with buzcode

--- sleepscoring/apply_state_thresh_across_sessions.m
+++ sleepscoring/apply_state_thresh_across_sessions.m
@@ -1,4 +1,4 @@
-function [swthresh, EMGthreh, THthresh] = apply_state_thresh_across_sessions(basename_new_session, basename_good_session)
+function [swthresh_new_norm, EMGthresh_new_norm, THthresh_new_norm] = apply_state_thresh_across_sessions(basename_new_session, basename_good_session)
 % [swthresh, EMGthreh, THthresh] = apply_state_thresh_across_sessions(basename_new_session, basename_good_session)
 %   Determine the appropriate thresholds to apply for "new_session" based
 %   on values obtained in "good_session" for determining sleep states.
@@ -12,18 +12,27 @@ function [swthresh, EMGthreh, THthresh] = apply_state_thresh_across_sessions(bas
 EMGgood = importdata([basename_good_session, '.EMGFromLFP.LFP.mat']);
 LFPgood = importdata([basename_good_session, '.SleepScoreLFP.LFP.mat']);
 
+disp('Running good session')
 [basePath_good, ~, ~] = fileparts(basename_good_session);
 [SleepScoreMetrics_good,~] = ClusterStates_GetMetrics(...
     basePath_good,LFPgood,EMGgood,false,'norm', false);
 
 swthresh_good = SleepScoreMetrics_good.histsandthreshs.swthresh;
 THthresh_good = SleepScoreMetrics_good.histsandthreshs.THthresh;
-EMGthresh_good = SleepScoreMetrics_good.histsandthreshs.EMGthresh;
+try
+    EMGthresh_good = SleepScoreMetrics_good.histsandthreshs.EMGthresh;
+    emg_or_motion = 'emg';
+catch
+    EMGthresh_good = SleepScoreMetrics_good.histsandthreshs.MotionThresh; 
+    emg_or_motion = 'motion';
+end
+
 
 %% Next run new_session through ClusterStates_GetMetrics and get data ranges
 EMGnew = importdata([basename_new_session, '.EMGFromLFP.LFP.mat']);
 LFPnew = importdata([basename_new_session, '.SleepScoreLFP.LFP.mat']);
 
+disp('Running new session')
 [basePath_new, ~, ~] = fileparts(basename_new_session);
 [SleepScoreMetrics_new,~] = ClusterStates_GetMetrics(...
     basePath_new,LFPnew,EMGnew,false,'norm', false);
@@ -31,15 +40,34 @@ LFPnew = importdata([basename_new_session, '.SleepScoreLFP.LFP.mat']);
 % Get data ranges
 swdatarange = [min(SleepScoreMetrics_new.broadbandSlowWave), 
     max(SleepScoreMetrics_new.broadbandSlowWave)];
-thdatarange = [min(SleepScoreMetrics_new.thratio), 
-    max(SleepScoreMetrics_new.thratio)];
-EMGdatarange = [min(SleepScoreMetrics_new.EMG), 
-    max(SleepScoreMetrics_new.EMG)];
+
+if strcmp(emg_or_motion, 'emg')
+    EMGdatarange = [min(SleepScoreMetrics_new.EMG), 
+        max(SleepScoreMetrics_new.EMG)];
+    EMGhistbins = SleepScoreMetrics_good.histsandthreshs.EMGhistbins;
+    EMGhist = SleepScoreMetrics_good.histsandthreshs.EMGhist;
+    EMGgood = SleepScoreMetrics_good.EMG;
+    EMGnew_use = SleepScoreMetrics_new.EMG;
+elseif strcmp(emg_or_motion, 'motion')
+    EMGdatarange = [min(SleepScoreMetrics_new.motiondata), 
+        max(SleepScoreMetrics_new.motiondata)];
+    EMGhistbins = SleepScoreMetrics_good.histsandthreshs.MotionHistBins;
+    EMGhist = SleepScoreMetrics_good.histsandthreshs.MotionHist;
+    EMGgood = SleepScoreMetrics_good.motiondata;
+    EMGnew_use = SleepScoreMetrics_new.motiondata;
+
+MOVtimes = (SleepScoreMetrics_new.broadbandSlowWave(:)<swthresh_good & EMGnew(:)>EMGthresh_good);
+thratio_new = SleepScoreMetrics_new.thratio(MOVtimes == 0);
+thdatarange = [min(thratio_new), max(thratio_new)];
 
 %% Third match up good thresholds to new data range after normalizing
 swthresh_new_norm = bz_NormToRange(swthresh_good, [0, 1], swdatarange);
-ththresh_new_norm = bz_NormToRange(THthresh_good, [0, 1], thdatarange);
+THthresh_new_norm = bz_NormToRange(THthresh_good, [0, 1], thdatarange);
 EMGthresh_new_norm = bz_NormToRange(EMGthresh_good, [0, 1], EMGdatarange);
+
+disp(['Use swthresh=' num2str(swthresh_new_norm) 'for new session'])
+disp(['Use THthresh=' num2str(THthresh_new_norm) 'for new session'])
+disp(['Use EMGthresh=' num2str(EMGthresh_new_norm) 'for new session'])
 
 %% Finally plot everything
 figure; subplot(2, 3, 1);
@@ -51,7 +79,7 @@ subplot(2, 3, 4);
 bin_size = mean(diff(SleepScoreMetrics_good.histsandthreshs.swhistbins));
 bins_use = SleepScoreMetrics_good.histsandthreshs.swhistbins - bin_size / 2;
 bins_use = [bins_use, bins_use(end) + bin_size];
-histogram(SleepScoreMetrics_good.broadbandSlowWave, bins_use);
+histogram(SleepScoreMetrics_new.broadbandSlowWave, bins_use);
 xline(swthresh_good, 'r');
 title('SW raw new')
 
@@ -61,24 +89,23 @@ xline(THthresh_good, 'r');
 title('TH raw good')
 
 subplot(2, 3, 5);
-MOVtimes = (SleepScoreMetrics_new.broadbandSlowWave(:)<swthresh_good & SleepScoreMetrics_new.EMG(:)>EMGthresh_good);
 bin_size = mean(diff(SleepScoreMetrics_good.histsandthreshs.THhistbins));
 bins_use = SleepScoreMetrics_good.histsandthreshs.THhistbins - bin_size / 2;
 bins_use = [bins_use, bins_use(end) + bin_size];
-histogram(SleepScoreMetrics_good.thratio(MOVtimes == 0), bins_use);
+histogram(thratio_new, bins_use);
 xline(THthresh_good, 'r');
 title('TH raw new')
 
 subplot(2, 3, 3);
-bar(SleepScoreMetrics_good.histsandthreshs.EMGhistbins, SleepScoreMetrics_good.histsandthreshs.EMGhist);
+bar(EMGhistbins, EMGhist);
 xline(EMGthresh_good, 'r');
 title('EMG raw good')
 
 subplot(2, 3, 6);
-bin_size = mean(diff(SleepScoreMetrics_good.histsandthreshs.EMGhistbins));
-bins_use = SleepScoreMetrics_good.histsandthreshs.EMGhistbins - bin_size / 2;
+bin_size = mean(diff(EMGhistbins));
+bins_use = EMGhistbins - bin_size / 2;
 bins_use = [bins_use, bins_use(end) + bin_size];
-histogram(SleepScoreMetrics_good.EMG, bins_use);
+histogram(EMGnew, bins_use);
 xline(EMGthresh_good, 'r');
 title('EMG raw new')
 
