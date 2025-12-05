87147d99cc22f8285a9a9df0c67e6c69005f928c
nkinsky <nkinsky@umich.edu>
2025-12-05 12:44:40 -0500

    add plot of normalized thresholds

--- sleepscoring/apply_state_thresh_across_sessions.m
+++ sleepscoring/apply_state_thresh_across_sessions.m
@@ -75,12 +75,12 @@ disp(['Use THthresh=' num2str(THthresh_new_norm) 'for new session'])
 disp(['Use EMGthresh=' num2str(EMGthresh_new_norm) 'for new session'])
 
 %% Finally plot everything
-figure; subplot(2, 3, 1);
+figure; subplot(3, 3, 1);
 bar(SleepScoreMetrics_good.histsandthreshs.swhistbins, SleepScoreMetrics_good.histsandthreshs.swhist);
 xline(swthresh_good, 'r');
 title('SW raw good')
 
-subplot(2, 3, 4);
+subplot(3, 3, 4);
 bin_size = mean(diff(SleepScoreMetrics_good.histsandthreshs.swhistbins));
 bins_use = SleepScoreMetrics_good.histsandthreshs.swhistbins - bin_size / 2;
 bins_use = [bins_use, bins_use(end) + bin_size];
@@ -88,12 +88,12 @@ histogram(SleepScoreMetrics_new.broadbandSlowWave, bins_use);
 xline(swthresh_good, 'r');
 title('SW raw new')
 
-subplot(2, 3, 2);
+subplot(3, 3, 2);
 bar(SleepScoreMetrics_good.histsandthreshs.THhistbins, SleepScoreMetrics_good.histsandthreshs.THhist);
 xline(THthresh_good, 'r');
 title('TH raw good')
 
-subplot(2, 3, 5);
+subplot(3, 3, 5);
 bin_size = mean(diff(SleepScoreMetrics_good.histsandthreshs.THhistbins));
 bins_use = SleepScoreMetrics_good.histsandthreshs.THhistbins - bin_size / 2;
 bins_use = [bins_use, bins_use(end) + bin_size];
@@ -101,12 +101,12 @@ histogram(thratio_new, bins_use);
 xline(THthresh_good, 'r');
 title('TH raw new')
 
-subplot(2, 3, 3);
+subplot(3, 3, 3);
 bar(EMGhistbins, EMGhist);
 xline(EMGthresh_good, 'r');
 title('EMG raw good')
 
-subplot(2, 3, 6);
+subplot(3, 3, 6);
 bin_size = mean(diff(EMGhistbins));
 bins_use = EMGhistbins - bin_size / 2;
 bins_use = [bins_use, bins_use(end) + bin_size];
@@ -115,5 +115,21 @@ xline(EMGthresh_good, 'r');
 title('EMG raw new')
 
 % NRK add in sanity check plotting of thresholds after normalizing!
+sublot(3,3,7);
+bins_use = SleepScoreMetrics_good.histsandthreshs.swhistbins;
+histogram(SleepScoreMetrics_new.broadbandSlowWave, length(bins_use) + 1);
+xline(swthresh_new_norm, 'r');
+title('SW raw new')
+
+subplot(3, 3, 8);
+bins_use = SleepScoreMetrics_good.histsandthreshs.THhistbins;
+histogram(thratio_new, length(bins_use) + 1);
+xline(THthresh_new_norm, 'r')
+title('TH raw new full')
+
+subplot(3, 3, 9);
+bins_use = EMGhistbins - bin_size / 2;
+histogram(EMGnew, length(bins_use));
+xline(EMGthresh_good, 'r');
 
 end
