38856c0f63c27224d02b061a449c7c2581ced2a3
Nat Kinsky <nkinsky@umich.edu>
2025-12-05 14:45:51 -0500

    Enhanced plotting of values and thresholds with sanity checks + bugfixes

--- sleepscoring/apply_state_thresh_across_sessions.m
+++ sleepscoring/apply_state_thresh_across_sessions.m
@@ -1,4 +1,5 @@
-function [swthresh_new_norm, EMGthresh_new_norm, THthresh_new_norm] = apply_state_thresh_across_sessions(basename_new_session, basename_good_session)
+function [swthresh_new_norm, EMGthresh_new_norm, THthresh_new_norm] = apply_state_thresh_across_sessions( ...
+    basename_new_session, basename_good_session, exclude_nrem_times)
 % [swthresh, EMGthreh, THthresh] = apply_state_thresh_across_sessions(basename_new_session, basename_good_session)
 %   Determine the appropriate thresholds to apply for "new_session" based
 %   on values obtained in "good_session" for determining sleep states.
@@ -6,6 +7,15 @@ function [swthresh_new_norm, EMGthresh_new_norm, THthresh_new_norm] = apply_stat
 %   Useful for setting realistic thresholds from a "good_session" with both
 %   sleep and wake for a "new_session" that has predominantly (or only) one
 %   type of state.
+%
+%  set 3rd argument ('exclude_nrem_times') to true if you see a warning pop
+%  up that says "No bimodal dip found in theta. Trying to exclude NREM..."
+%  while running.
+
+% don't exclude nrem times by default
+if nargin < 3
+    exclude_nrem_times = false;
+end
 
 %% First run good_session through ClusterStates_GetMetrics without normalizing EMG, SW, and theta arrays
 
@@ -17,13 +27,14 @@ disp('Running good session')
 [SleepScoreMetrics_good,~] = ClusterStates_GetMetrics(...
     basePath_good,LFPgood,EMGgood,false,'norm', false);
 
+% Get thresholds
 swthresh_good = SleepScoreMetrics_good.histsandthreshs.swthresh;
 THthresh_good = SleepScoreMetrics_good.histsandthreshs.THthresh;
 try
     EMGthresh_good = SleepScoreMetrics_good.histsandthreshs.EMGthresh;
     emg_or_motion = 'emg';
 catch
-    EMGthresh_good = SleepScoreMetrics_good.histsandthreshs.MotionThresh; 
+    EMGthresh_good = SleepScoreMetrics_good.histsandthreshs.MotionThresh;
     emg_or_motion = 'motion';
 end
 
@@ -37,21 +48,28 @@ disp('Running new session')
 [SleepScoreMetrics_new,~] = ClusterStates_GetMetrics(...
     basePath_new,LFPnew,EMGnew,false,'norm', false);
 
+% Get good and new EMG thresholds
+
+
 % Get data ranges
 swdatarange = [min(SleepScoreMetrics_new.broadbandSlowWave), 
     max(SleepScoreMetrics_new.broadbandSlowWave)];
 
 if strcmp(emg_or_motion, 'emg')
+    EMGthresh_new_orig = SleepScoreMetrics_new.histsandthreshs.EMGthresh;
     EMGdatarange = [min(SleepScoreMetrics_new.EMG), 
         max(SleepScoreMetrics_new.EMG)];
     EMGhistbins = SleepScoreMetrics_good.histsandthreshs.EMGhistbins;
+    EMGhistbins_orig = SleepScoreMetrics_new.histsandthreshs.EMGhistbins;
     EMGhist = SleepScoreMetrics_good.histsandthreshs.EMGhist;
     EMGgood = SleepScoreMetrics_good.EMG;
     EMGnew_use = SleepScoreMetrics_new.EMG;
 elseif strcmp(emg_or_motion, 'motion')
+    EMGthresh_new_orig = SleepScoreMetrics_new.histsandthreshs.MotionThresh;
     EMGdatarange = [min(SleepScoreMetrics_new.motiondata), 
         max(SleepScoreMetrics_new.motiondata)];
     EMGhistbins = SleepScoreMetrics_good.histsandthreshs.MotionHistBins;
+    EMGhistbins_orig = SleepScoreMetrics_new.histsandthreshs.MotionHistBins;
     EMGhist = SleepScoreMetrics_good.histsandthreshs.MotionHist;
     EMGgood = SleepScoreMetrics_good.motiondata;
     EMGnew_use = SleepScoreMetrics_new.motiondata;
@@ -76,60 +94,119 @@ disp(['Use EMGthresh=' num2str(EMGthresh_new_norm) 'for new session'])
 
 %% Finally plot everything
 figure; subplot(3, 3, 1);
-bar(SleepScoreMetrics_good.histsandthreshs.swhistbins, SleepScoreMetrics_good.histsandthreshs.swhist);
-xline(swthresh_good, 'r');
-title('SW raw good')
+hg = bar(SleepScoreMetrics_good.histsandthreshs.swhistbins, ...
+    SleepScoreMetrics_good.histsandthreshs.swhist / sum(SleepScoreMetrics_good.histsandthreshs.swhist));
+xlabel('Broadband SW')
+ylabel('Probability')
+title('Both sessions: Raw values')
 
-subplot(3, 3, 4);
-bin_size = mean(diff(SleepScoreMetrics_good.histsandthreshs.swhistbins));
-bins_use = SleepScoreMetrics_good.histsandthreshs.swhistbins - bin_size / 2;
-bins_use = [bins_use, bins_use(end) + bin_size];
-histogram(SleepScoreMetrics_new.broadbandSlowWave, bins_use);
-xline(swthresh_good, 'r');
-title('SW raw new')
+for j = 1:2
+    norm_type = 'probability';
+    if j == 1; hold on; else; subplot(3, 3, 4); norm_type = 'count'; end
+    bin_size = mean(diff(SleepScoreMetrics_good.histsandthreshs.swhistbins));
+    bins_use = SleepScoreMetrics_good.histsandthreshs.swhistbins - bin_size / 2;
+    bins_use = [bins_use, bins_use(end) + bin_size];
+    hn = histogram(SleepScoreMetrics_new.broadbandSlowWave, bins_use, 'Normalization', norm_type);
+    ht = xline(swthresh_good, 'r');
+    if j == 1
+        legend([hg, hn, ht], ["Good", "New", "Raw thresh"]);
+    else 
+        title('New session only: Raw values'); 
+        xlabel('Broadband SW'); 
+        ylabel('Count');
+    end
+end
+% title('SW raw new')
 
 subplot(3, 3, 2);
-bar(SleepScoreMetrics_good.histsandthreshs.THhistbins, SleepScoreMetrics_good.histsandthreshs.THhist);
-xline(THthresh_good, 'r');
-title('TH raw good')
+hg = bar(SleepScoreMetrics_good.histsandthreshs.THhistbins, ...
+    SleepScoreMetrics_good.histsandthreshs.THhist / sum(SleepScoreMetrics_good.histsandthreshs.THhist));
+xlabel('Theta / all ratio')
+ylabel('Probability')
+title('Both sessions: Raw values')
 
-subplot(3, 3, 5);
-bin_size = mean(diff(SleepScoreMetrics_good.histsandthreshs.THhistbins));
-bins_use = SleepScoreMetrics_good.histsandthreshs.THhistbins - bin_size / 2;
-bins_use = [bins_use, bins_use(end) + bin_size];
-histogram(thratio_new, bins_use);
-xline(THthresh_good, 'r');
-title('TH raw new')
+for j = 1:2
+    norm_type = 'probability';
+    if j == 1; hold on; else; subplot(3, 3, 5); norm_type = 'count'; end
+
+    bin_size = mean(diff(SleepScoreMetrics_good.histsandthreshs.THhistbins));
+    bins_use = SleepScoreMetrics_good.histsandthreshs.THhistbins - bin_size / 2;
+    bins_use = [bins_use, bins_use(end) + bin_size];
+    hn = histogram(thratio_new, bins_use, 'Normalization', norm_type);
+    ht = xline(THthresh_good, 'r');
+    if j == 1
+        legend([hg, hn, ht], ["Good", "New", "Raw thresh"]);
+    else 
+        title('New session only: Raw values'); 
+        xlabel('Theta / all ratio'); 
+        ylabel('Count');
+    end
+end
 
 subplot(3, 3, 3);
-bar(EMGhistbins, EMGhist);
+hg = bar(EMGhistbins, EMGhist / sum(EMGhist));
 xline(EMGthresh_good, 'r');
-title('EMG raw good')
+xlabel('EMG/Motion')
+ylabel('Probability')
+title('Both sessions: Raw values')
 
-subplot(3, 3, 6);
-bin_size = mean(diff(EMGhistbins));
-bins_use = EMGhistbins - bin_size / 2;
-bins_use = [bins_use, bins_use(end) + bin_size];
-histogram(EMGnew_use, bins_use);
-xline(EMGthresh_good, 'r');
-title('EMG raw new')
+for j = 1:2
+    norm_type = 'probability';
+    if j == 1; hold on; else; subplot(3, 3, 6); norm_type = 'count'; end
+    bin_size = mean(diff(EMGhistbins));
+    bins_use = EMGhistbins - bin_size / 2;
+    bins_use = [bins_use, bins_use(end) + bin_size];
+    hn = histogram(EMGnew_use, bins_use, 'Normalization', norm_type);
+    ht = xline(EMGthresh_good, 'r');
+    if j == 1
+        legend([hg, hn, ht], ["Good", "New", "Raw thresh"]);
+    else 
+        title('New session only: Raw values'); 
+        xlabel('EMG/Motion'); 
+        ylabel('Count');
+    end
+end
 
-% NRK add in sanity check plotting of thresholds after normalizing!
-sublot(3,3,7);
-bins_use = SleepScoreMetrics_good.histsandthreshs.swhistbins;
-histogram(SleepScoreMetrics_new.broadbandSlowWave, length(bins_use) + 1);
-xline(swthresh_new_norm, 'r');
-title('SW raw new')
+% Sanity check plotting of data and thresholds after normalizing!
+subplot(3, 3, 7);
+swthresh_orig_norm = bz_NormToRange(SleepScoreMetrics_new.histsandthreshs.swthresh, ...
+    [0, 1], swdatarange);
+histogram(bz_NormToRange(SleepScoreMetrics_new.broadbandSlowWave, [0, 1]), ...
+    bz_NormToRange(SleepScoreMetrics_new.histsandthreshs.swhistbins, [0, 1], swdatarange))
+hn = xline(swthresh_new_norm, 'r');
+horig = xline(swthresh_orig_norm, 'g--');
+title('New session: normalized values')
+xlabel('Broadband SW')
+ylabel('Count')
+legend([hn, horig], ["New thresh", "Orig thresh"]);
 
 subplot(3, 3, 8);
-bins_use = SleepScoreMetrics_good.histsandthreshs.THhistbins;
-histogram(thratio_new, length(bins_use) + 1);
-xline(THthresh_new_norm, 'r')
-title('TH raw new full')
+MOVtimes_orig = ((SleepScoreMetrics_new.broadbandSlowWave(:)<SleepScoreMetrics_new.histsandthreshs.swthresh) ...
+    & (EMGnew_use(:) > EMGthresh_new_orig));
+thratio_new_orig = SleepScoreMetrics_new.thratio(MOVtimes_orig == 0);
+% THthresh_orig_norm = bz_NormToRange(SleepScoreMetrics_new.histsandthreshs.THthresh, ...
+%     [0,1], SleepScoreMetrics_new.histsandthreshs.THhistbins([1, end]));
+THthresh_orig_norm = bz_NormToRange(SleepScoreMetrics_new.histsandthreshs.THthresh, ...
+    [0,1], [min(thratio_new_orig), max(thratio_new_orig)]);
+histogram(bz_NormToRange(thratio_new_orig, [0, 1]), ...
+    bz_NormToRange(SleepScoreMetrics_new.histsandthreshs.THhistbins, [0, 1], thdatarange));
+hn = xline(THthresh_new_norm, 'r');
+horig = xline(THthresh_orig_norm, 'g--');
+xlabel('TH / all ratio')
+ylabel('Count')
+title('New session: normalized values')
+legend([hn, horig], ["New thresh", "Orig thresh"]);
 
 subplot(3, 3, 9);
-bins_use = EMGhistbins - bin_size / 2;
-histogram(EMGnew, length(bins_use));
-xline(EMGthresh_good, 'r');
+EMGthresh_orig_norm = bz_NormToRange(EMGthresh_new_orig, [0, 1], EMGdatarange);
+histogram(bz_NormToRange(EMGnew_use, [0, 1]), ...
+    bz_NormToRange(EMGhistbins_orig, [0, 1], EMGdatarange));
+hn = xline(EMGthresh_good, 'r');
+horig = xline(EMGthresh_orig_norm, 'g--');
+xlabel('EMG / Motion')
+ylabel('Count')
+title('New session: normalized values')
+legend([hn, horig], ["New thresh", "Orig thresh"]);
+
 
 end
